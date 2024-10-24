d1ed5b18fb998259a244ebb097b4682f2465c93a
enjoy2003 <joypijitra2546@gmail.com>
2024-10-24 10:51:35 +0700

    test

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'test2'
+  database: process.env.DB_NAME || 'test'
 });
 
 // Create a new Express app
